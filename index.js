9c7f8edefc47c5b85e3f3778b382611a62a14d7c
unknown <luqman.hadi85@gmail.com>
2023-07-26 18:29:02 +0700

    changed code structure to be the main server

--- index.js
+++ index.js
@@ -2,90 +2,36 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const mongoose = require('mongoose');
-const {data_video, data_komen_video, data_product_video} = require('./model/data_app');
+
+const videoController = require('./controllers/videoController');
+const commentController = require('./controllers/commentController');
+const videoView = require('./views/videoView');
+
 const port = 3000;
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://127.0.0.1/app_db')
-.then((response) => {
+  .then((response) => {
     console.log("Berhasil konek ke database");
-})
-.catch((err) => {
+  })
+  .catch((err) => {
     console.log(`Error: ${err}`);
-})
+  });
 
 app.use(cors());
 
-app.get('/', async (req, res) => {
-    try {
-        const result = await data_video.findOne();
-        const videoData = result.videoData.map(object_video => ({
-            "videoID": object_video.videoID,
-            "URLthumbnail": object_video.URLthumbnail
-        }));
-        res.json(videoData);
-    } catch (error) {
-        res.json(error);
-    }
-})
+// Routes for video data
+app.get('/', videoController.getAllVideos); 
 
-app.get('/video/:id', async (req, res) => {
-    try {
-        const id = req.params.id;
-        const result = await data_product_video.findOne({videoID: id});
-        const productData = result.products.map(object_product => ({
-            "productID": object_product.productID,
-            "productLink": object_product.productLink,
-            "productTitle": object_product.productTitle,
-            "productPrice": object_product.productPrice
-        }))
-        res.json(productData);
-    } catch (error) {
-        res.json(error);
-    }
-})
+app.get('/video/:id', videoController.getProduct); 
 
-app.get('/video/:id/comments', async (req, res) => {
-    try {
-        const id = req.params.id;
-        const result = await data_komen_video.find({"videoID": id});
-        res.json(result); //ide: bikin komen 1 vid kedalem array of object, 1 object isinya data komen dari 1 user
-    } catch (error) {
-        res.json(error);
-    }
-})
+// Routes for comments
+app.get('/video/:id/comments', commentController.getComments); 
 
-app.post('/video/:id/comments/post', async (req, res) => {
-    try {
-        const ID = req.params.id;
-        const {Name, Comment} = req.body;
-        if (!Name || !Comment){
-            res.status(400).send("Client side error: :Lack of parameters");
-        }
-        else{
-            data_komen_video.findOneAndUpdate({"videoID": ID}, 
-            {$push: {"comments": [{
-                "username":Name, 
-                "comment":Comment, 
-                "timestamp": new Date().toString()}]}}, {new: true})
-            .then(updatedDoc => {
-                console.log("Document Updated Successfully");
-            })
-            .catch(error => {
-                console.error("Error Occurred:", error);
-            });
-            const result = await data_komen_video.findOne({videoID: ID});
-            console.log(result);
-            res.status(200).send(result);
-        }
-    } catch (error) {
-        console.log(error);
-        res.send("Gagal");
-    }
-})
+app.post('/video/:id/comments/post', commentController.postComment); 
 
 app.listen(port, () => {
-    console.log(`Server berjalan pada port ${port}`);
-})
+  console.log(`Server berjalan pada port ${port}`);
+});
