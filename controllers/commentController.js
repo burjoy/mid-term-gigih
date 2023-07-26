2a987b51ff0fa82b70c6a3466d604f2a838b2869
unknown <luqman.hadi85@gmail.com>
2023-07-26 18:31:08 +0700

    added controller to handle get and post comment

---
+++ controllers/commentController.js
@@ -0,0 +1,56 @@
+const { data_komen_video } = require('../models/data_app');
+const commentView = require('../views/videoView');
+
+// Controller function to handle the route for fetching comments by video ID
+async function getComments(req, res) {
+  try {
+    const id = req.params.id;
+    const result = await data_komen_video.find({ "videoID": id });
+    commentView.renderComments(res, result);
+  } catch (error) {
+    res.json(error);
+  }
+}
+
+// Controller function to handle the route for posting a new comment
+async function postComment(req, res) {
+  try {
+    const ID = req.params.id;
+    const { Name, Comment } = req.body;
+    if (!Name || !Comment) {
+      res.status(400).send("Client side error: Lack of parameters");
+    } else {
+      // Update the comments in the database
+      data_komen_video.findOneAndUpdate({ "videoID": ID },
+        {
+          $push: {
+            "comments": [{
+              "username": Name,
+              "comment": Comment,
+              "timestamp": new Date().toString()
+            }]
+          }
+        },
+        { new: true }
+      )
+        .then(updatedDoc => {
+          console.log("Document Updated Successfully");
+        })
+        .catch(error => {
+          console.error("Error Occurred:", error);
+        });
+
+      // Fetch and return the updated comments after posting
+      const result = await data_komen_video.findOne({ videoID: ID });
+      commentView.renderComments(res, result);
+    }
+  } catch (error) {
+    console.log(error);
+    res.send("Gagal");
+  }
+}
+
+module.exports = {
+  getComments,
+  postComment,
+};
