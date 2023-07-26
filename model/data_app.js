9457dce86a93c6e0ad9a651789adea7a02312dd9
unknown <luqman.hadi85@gmail.com>
2023-07-26 16:55:30 +0700

    added new schema and new collections

--- model/data_app.js
+++ model/data_app.js
@@ -1,25 +1,32 @@
 const mongoose = require('mongoose');
 
-const format_data_aplikasi = new mongoose.Schema({
-    videoID: {
-        type: String,
-        required: true
-    },
-    URLthumbnail: {
-        type: String,
-        required: true
-    },
-    products: [{
+const format_data_video = new mongoose.Schema({
+    videoData: [{
+        videoID: String,
+        URLthumbnail: String
+    }]})
+
+const format_komen_video = mongoose.Schema({
+        videoID: String,
+        comments: [{
+            username: String,
+            comment: String,
+            timestamp: String
+        }]
+    });
+
+const format_product_video = mongoose.Schema({
+    videoID: String,
+   products: [{
         productID: String,
         productLink: String,
         productTitle: String,
         productPrice: Number
-    }],
-    comments: [{userName: String,
-                userComment: String,
-                timestamp: String}]
-});
+    }]
+})
 
-const data_aplikasi = mongoose.model("db_aplikasi", format_data_aplikasi);
+const data_video = mongoose.model("data_video", format_data_video, "data_video");
+const data_komen_video = mongoose.model("data_komen_video", format_komen_video, "data_komen_video");
+const data_product_video = mongoose.model("data_product_video", format_product_video, "data_produk_video");
 
-module.exports = data_aplikasi;
+module.exports = {data_video, data_komen_video, data_product_video};
