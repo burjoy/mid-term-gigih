096bb9fd57095d79b1512b7b4d8ae76d9edc11c5
Luqman <51292479+burjoy@users.noreply.github.com>
2023-08-15 18:46:55 +0700

    deleted apis directory and moved to server

--- apis/getAllVideo.js
+++
@@ -1,8 +0,0 @@
-async function getAllVideos() {
-    const ambil_data = await fetch("http://localhost:3000");
-    const response = await ambil_data.json();
-    console.log(response);
-    return response
-}
-
-export default getAllVideos
--- apis/getProducts.js
+++
@@ -1,9 +0,0 @@
-async function getProducts(id){
-    const encodedUrl = encodeURI(id);
-    const ambil_data = await fetch(`http://localhost:3000/video/${encodedUrl}`);
-    const response = await ambil_data.json();
-    console.log(response);
-    return response
-}
-
-export default getProducts
