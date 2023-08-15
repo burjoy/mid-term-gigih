11c006d8e059e557e6d2bfd28887823a346536a5
Luqman <51292479+burjoy@users.noreply.github.com>
2023-08-15 19:26:21 +0700

    Update getComments.js route

--- server/apis/getComments.js
+++ server/apis/getComments.js
@@ -1,6 +1,6 @@
 async function getComments(id){
     const encodedUrl = encodeURI(id);
-    const ambil_data = await fetch(`http://localhost:3000/video/${encodedUrl}/comments`);
+    const ambil_data = await fetch(`https://mid-term-gigih-api.vercel.app/video/${encodedUrl}/comments`);
     const response = await ambil_data.json();
     console.log(response);
     return response
