6ee30e241e18aa29329bd73b42978320337231ca
Luqman <51292479+burjoy@users.noreply.github.com>
2023-08-15 19:26:40 +0700

    Update getProducts.js route

--- server/apis/getProducts.js
+++ server/apis/getProducts.js
@@ -1,6 +1,6 @@
 async function getProducts(id){
     const encodedUrl = encodeURI(id);
-    const ambil_data = await fetch(`http://localhost:3000/video/${encodedUrl}`);
+    const ambil_data = await fetch(`https://mid-term-gigih-api.vercel.app/video/${encodedUrl}`);
     const response = await ambil_data.json();
     console.log(response);
     return response
